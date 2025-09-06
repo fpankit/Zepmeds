adb803f5b4f0fb73c5b14e01989e3919166a9033
Ankit Srivastav <ankitop987@gmail.com>
2025-09-06 14:32:43 +0000

    VM1691 lockdown-install.js:1 SES_UNCAUGHT_EXCEPTION: null (anonymous) @

--- next.config.ts
+++ next.config.ts
@@ -18,23 +18,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
