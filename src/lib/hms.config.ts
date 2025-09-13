c26fc361accf93da88d799256e40e3e7c8c391c9
Ankit Srivastav <ankitop987@gmail.com>
2025-09-13 04:27:12 +0000

    App secret : cQXWcHAAWOcrZDnrwsEx1zvAsg35yS5BhX51VKgsYvI5rb6yVE6F4s68WZI

--- src/lib/hms.config.ts
+++ src/lib/hms.config.ts
@@ -2,6 +2,6 @@
 // This is a temporary solution for a development environment.
 
 export const HMS_CONFIG = {
-  ACCESS_KEY: 'REPLACE_WITH_YOUR_ACCESS_KEY',
-  SECRET: 'REPLACE_WITH_YOUR_SECRET',
+  ACCESS_KEY: '68c3b6c6bd0dab5f9a013bea',
+  SECRET: 'cQXWcHAAWOcrZDnrwsEx1zvAsg35yS5BhX51VKgsYvI5rb6yVE6F4s68WZIYDkgYdBWD-mPZLaSCYIjBShN3MBKm9S-ujB8OJZnAtKGDL1SKBjpxdBwtKwIAXvy7iZAbqNMfe3w5pL0fXHy2KEtWgh4FSqj4mljdA1y3S2N8nTA=',
 };
