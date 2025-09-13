17d2e6c04a72c49da74642b8d8be535108463658
Ankit Srivastav <ankitop987@gmail.com>
2025-09-13 04:29:40 +0000

    and the updates in the env file?

--- src/lib/hms.config.ts
+++ src/lib/hms.config.ts
@@ -1,7 +1,7 @@
-// WARNING: Storing secrets directly in source code is not recommended for production.
-// This is a temporary solution for a development environment.
+// It is recommended to store secrets in an environment file.
+// This configuration reads from your .env file.
 
 export const HMS_CONFIG = {
-  ACCESS_KEY: '68c3b6c6bd0dab5f9a013bea',
-  SECRET: 'cQXWcHAAWOcrZDnrwsEx1zvAsg35yS5BhX51VKgsYvI5rb6yVE6F4s68WZIYDkgYdBWD-mPZLaSCYIjBShN3MBKm9S-ujB8OJZnAtKGDL1SKBjpxdBwtKwIAXvy7iZAbqNMfe3w5pL0fXHy2KEtWgh4FSqj4mljdA1y3S2N8nTA=',
+  ACCESS_KEY: process.env.HMS_ACCESS_KEY,
+  SECRET: process.env.HMS_SECRET,
 };
