c9bf1061ee44841bdd80ab155e821026f1564093
Ankit Srivastav <ankitop987@gmail.com>
2025-09-15 15:19:01 +0000

    aur apne saare me krdu na ye??

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,14 @@ import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// PASTE YOUR NEW FIREBASE CONFIGURATION OBJECT HERE
 const firebaseConfig = {
-  apiKey: "AIzaSyDpYF1VFWTjxRRXsx_hQAUBvbZetv945VE",
-  authDomain: "zepmeds-admin-panel.firebaseapp.com",
-  projectId: "zepmeds-admin-panel",
-  storageBucket: "zepmeds-admin-panel.appspot.com",
-  messagingSenderId: "731788163738",
-  appId: "1:731788163738:web:644ab0e7387be742fefd23"
+  apiKey: "YOUR_NEW_API_KEY",
+  authDomain: "YOUR_NEW_AUTH_DOMAIN",
+  projectId: "YOUR_NEW_PROJECT_ID",
+  storageBucket: "YOUR_NEW_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_NEW_MESSAGING_SENDER_ID",
+  appId: "YOUR_NEW_APP_ID"
 };
 
 // Initialize Firebase
