102da5f63c2f50036ee6657336c2db89b418de90
Ankit Srivastav <ankitop987@gmail.com>
2025-09-17 08:12:29 +0000

    but ye to browser me khulga,and offline features ke liye net band krna p

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+
 const nextConfig = {
   /* config options here */
   typescript: {
@@ -46,4 +60,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
