697b5fc30a3456954c0ff4181ce6b6c9321981fa
Ankit Srivastav <ankitop987@gmail.com>
2025-09-25 05:29:40 +0000

    dekh jab vercel pe deploy krke dekh rha hu na, to sirf firebase ka hi lo

---
+++ src/app/icon.tsx
@@ -0,0 +1,56 @@
+import { ImageResponse } from 'next/og'
+import { Logo } from '@/components/icons/logo';
+
+// Route segment config
+export const runtime = 'edge'
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 24,
+          background: '#09090b', // dark background
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          borderRadius: '4px',
+        }}
+      >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 200 200"
+            width="180"
+            height="180"
+        >
+            <path
+                d="M50 100 C 65 50, 95 50, 110 100 S 140 150, 155 100"
+                stroke="#2563eb"
+                strokeWidth="12"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported icons size metadata
+      // config, but overriding the width and height is optional.
+      ...size,
+    }
+  )
+}
